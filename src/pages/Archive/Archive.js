37804141c9d6317901a1b256de1f35911cf7425b
Mauro <mauro@osn.global>
2020-07-27 19:10:01 -0300

    Add loading component to Archive

--- src/pages/Archive/Archive.js
+++ src/pages/Archive/Archive.js
@@ -1,6 +1,27 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+
 import TweetList from 'components/TweetList';
+import Loading from 'components/Loading';
+
+const ArchiveContainer = styled.div`
+  padding: ${props => props.theme.spacing(3)}px auto;
+`;
+
+const LoadingTweets = styled.div`
+  margin: ${props => props.theme.spacing(5)}px auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoadingText = styled(Typography)`
+  color: ${props => props.theme.palette.primary.light};
+  margin-top: ${props => props.theme.spacing(1)}px;
+`;
 
 const Archive = () => {
   const [tweets, setTweets] = useState(null);
@@ -13,8 +34,18 @@ const Archive = () => {
     fetchTweets();
   }, []);
 
-  if (tweets === null) return null;
-  return <TweetList tweets={tweets} />;
+  return (
+    <ArchiveContainer>
+      {tweets ? (
+        <TweetList tweets={tweets} />
+      ) : (
+        <LoadingTweets>
+          <Loading />
+          <LoadingText variant="h5">Fetching Archive</LoadingText>
+        </LoadingTweets>
+      )}
+    </ArchiveContainer>
+  );
 };
 
 export default Archive;
